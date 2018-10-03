5f4e3451ae1ac220e3ebdaf802aec4c4fbe7392b
Muhammad Umer Khan <um4r12@gmail.com>
2018-10-02 18:02:53 -0600

    adding javadocs

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,7 +1,16 @@
 package ca.ualberta.cs.lonelytwitter;
 
 /**
- * Created by lipton on 9/18/18.
+ * An abstract class which returns a Tweet object that can be used to display a series of tweets
+ * It initializes the message with a default value if it is not provided.
+ * Otherwise, it will be intialized to the input string, as long as it is
+ * under 140 characters.
+ * The date is initialized to upon initialization
+ *
+ * @author Joshua TA
+ * @return  a tweet object
+ * @version v3
+ * @see ca.ualberta.cs.lonelytwitter.Tweetable
  */
 
 import java.util.Date;
@@ -14,14 +23,28 @@ public abstract class Tweet implements Tweetable {
 
     // Need to make constructor to have new tweets now
     // constructor is name of class itself
-
+    /**
+     * The Tweet constructor initializes the Date with a current date, and sets a default
+     * tweet message.
+     *
+     * @param
+     * @return
+     *
+     */
     Tweet() {
         // Need to initialize everything, set everything that exists for that object
         this.date = new Date();    // this is like self in python
         this.message = "I am default message"; //all would have same message
 
     }
-
+    /**
+     * The Tweet constructor initializes the Date with a current date, and sets the
+     * tweet message with the string provided, given that it is less than 140 chars.
+     *
+     * @param   message an optional string argument that must less than 140 characters
+     * @return
+     *
+     */
     Tweet(String message) {
         this.date = new Date();
         this.message = message; // Tweet myTweet = new Tweet("this is new")
